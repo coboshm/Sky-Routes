2ce3d02f4b78368dace35c582ff118d179db7f43
Marc Cobos <mcobos@trovit.com>
2016-02-27 21:21:34 +0100

    Fix Cache folder and composer

--- config/production.php
+++ config/production.php
@@ -1,5 +1,5 @@
 <?php
 
 $app['twig.path']       = array(__DIR__ . '/../template');
-$app['twig.options']    = array('cache' => __DIR__ . '/../cache/twig');
+$app['twig.options']    = array('cache' => __DIR__ . '/../var/cache/twig');
 $app['googleAnalytics'] = 'UA-XXXXXXXX-X';
