398a8f62d1a336af046511c4c95fa7e48d9168fe
Marc Cobos <mcobos@trovit.com>
2016-02-28 00:06:38 +0100

    More CSS and JS

--- web/assets/js/angularControllers/searchController.js
+++ web/assets/js/angularControllers/searchController.js
@@ -75,8 +75,6 @@ myApp.controller('searchCtrl', function($scope, $http, $uibModal) {
         if( navigator.userAgent.match(/Android/i)
             || navigator.userAgent.match(/webOS/i)
             || navigator.userAgent.match(/iPhone/i)
-            || navigator.userAgent.match(/iPad/i)
-            || navigator.userAgent.match(/iPod/i)
             || navigator.userAgent.match(/BlackBerry/i)
             || navigator.userAgent.match(/Windows Phone/i)
         ){
